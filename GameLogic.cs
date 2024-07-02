18cde6a408bc257351943f745635b793a69a7ab2
TVP <tomas.victor.pettersson@outlook.com>
2024-07-02 23:43:50 +0200

    "Made the following changes in GameLogic.cs: Renamed methods and variables to clarify the code, added Console.Clear at some places and improved the High Score board"

--- GameLogic.cs
+++ GameLogic.cs
@@ -9,7 +9,7 @@ namespace MooGame
 			string userName;
 			do
 			{
-				Console.WriteLine("Enter your user name:\n");
+				Console.Write("Enter your user name: ");
 				userName = Console.ReadLine()!;
 				if (string.IsNullOrEmpty(userName))
 				{
@@ -23,26 +23,28 @@ namespace MooGame
 
 		public static void PlayGame(string userName)
 		{
-			string goal = MakeGoal();
+			Console.Clear();
+			string secretNumber = MakeSecretNumber();
 			Console.WriteLine("New game:\n");
-			//comment out or remove next line to play real games!
-			Console.WriteLine($"For practice, number is: {goal}\n");
+
+			////comment out or remove next line to play real games!
+			Console.WriteLine($"For practice, number is: {secretNumber}\n");
 			string guess = Console.ReadLine()!;
 
 			int numberOfGuesses = 1;
-			string bbcc = CheckBC(goal, guess);
-			Console.WriteLine($"{bbcc}\n");
-			while (!string.Equals(bbcc, "BBBB,", StringComparison.OrdinalIgnoreCase))
+			string guessFeedback = GenerateBullsAndCowsFeedback(secretNumber, guess);
+			Console.WriteLine($"{guessFeedback}\n");
+			while (!string.Equals(guessFeedback, "BBBB,", StringComparison.OrdinalIgnoreCase))
 			{
 				numberOfGuesses++;
 				guess = Console.ReadLine()!;
 				Console.WriteLine($"{guess}\n");
-				bbcc = CheckBC(goal, guess);
-				Console.WriteLine($"{bbcc}\n");
+				guessFeedback = GenerateBullsAndCowsFeedback(secretNumber, guess);
+				Console.WriteLine($"{guessFeedback}\n");
 			}
 			SaveResult(userName, numberOfGuesses);
 			ShowTopList();
-			Console.WriteLine($"Correct, it took {numberOfGuesses}");
+			Console.WriteLine($"\nCorrect, it took {numberOfGuesses} guesses");
 		}
 
 		private static void SaveResult(string userName, int numberOfGuesses)
@@ -51,27 +53,27 @@ namespace MooGame
 			output.WriteLine($"{userName}#&#{numberOfGuesses}");
 		}
 
-		private static string MakeGoal()
+		private static string MakeSecretNumber()
 		{
 			Random randomGenerator = new();
-			StringBuilder goal = new();
+			StringBuilder secretNumber = new();
 
 			for (int i = 0; i < 4; i++)
 			{
 				int random = randomGenerator.Next(10);
 				string randomDigit = $"{random}";
 
-				while (goal.ToString().Contains(randomDigit))
+				while (secretNumber.ToString().Contains(randomDigit))
 				{
 					random = randomGenerator.Next(10);
 					randomDigit = $"{random}";
 				}
-				goal.Append(randomDigit);
+				secretNumber.Append(randomDigit);
 			}
-			return goal.ToString();
+			return secretNumber.ToString();
 		}
 
-		private static string CheckBC(string goal, string guess)
+		private static string GenerateBullsAndCowsFeedback(string secretNumber, string guess)
 		{
 			int cows = 0, bulls = 0;
 			guess += "    ";     // if player entered less than 4 chars
@@ -79,7 +81,7 @@ namespace MooGame
 			{
 				for (int j = 0; j < 4; j++)
 				{
-					if (goal[i] == guess[j])
+					if (secretNumber[i] == guess[j])
 					{
 						if (i == j)
 						{
@@ -99,6 +101,10 @@ namespace MooGame
 
 		private static void ShowTopList()
 		{
+			Console.Clear();
+			Console.WriteLine("=== High Score List ===");
+			Console.WriteLine("Player       Games   Average Guesses");
+			Console.WriteLine("------------------------------------");
 			using StreamReader input = new("result.txt");
 			List<PlayerData> results = [];
 			string line;
@@ -108,7 +114,7 @@ namespace MooGame
 				string userName = userNameAndUserScore[0];
 				int guesses = Convert.ToInt32(userNameAndUserScore[1]);
 				PlayerData pd = new(userName, guesses);
-				int pos = results.IndexOf(pd);
+				int pos = results.FindIndex(p => p.Equals(pd));
 				if (pos < 0)
 				{
 					results.Add(pd);
@@ -118,12 +124,11 @@ namespace MooGame
 					results[pos].AddGuess(guesses);
 				}
 			}
-			results.Sort((p1, p2) => p1.CalculateAverageGuesses().
-			CompareTo(p2.CalculateAverageGuesses()));
-			Console.WriteLine("Player   games average");
+			results.Sort((p1, p2) => p1.CalculateAverageGuesses().CompareTo(p2.CalculateAverageGuesses()));
+
 			foreach (PlayerData p in results)
 			{
-				Console.WriteLine($"{p.UserName,-9}{p.TotalGamesPlayed,5:D}{p.CalculateAverageGuesses(),9:F2}");
+				Console.WriteLine($"{p.UserName,-12}{p.TotalGamesPlayed,7}{p.CalculateAverageGuesses(),14:F2}");
 			}
 		}
 
@@ -131,15 +136,16 @@ namespace MooGame
 		{
 			while (true)
 			{
-				Console.WriteLine("Continue? (y/n)");
+				Console.Write("\nContinue? (y/n): ");
 				string answer = Console.ReadLine()!;
 
 				if (string.IsNullOrEmpty(answer))
 				{
-					Console.WriteLine("Empty values are not allowed. Please enter y or n.");
+					Console.WriteLine("Empty values are not allowed. Please enter y for yes or n for no.");
 				}
 				else if (string.Equals(answer, "y", StringComparison.OrdinalIgnoreCase))
 				{
+					Console.Clear();
 					return true;
 				}
 				else if (string.Equals(answer, "n", StringComparison.OrdinalIgnoreCase))
@@ -148,7 +154,7 @@ namespace MooGame
 				}
 				else
 				{
-					Console.WriteLine("Invalid input. Please enter y or n.");
+					Console.WriteLine("Invalid input. Please enter y for yes or n for no.");
 				}
 			}
 		}
