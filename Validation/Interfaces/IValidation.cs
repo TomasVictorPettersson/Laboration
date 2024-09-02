9d146d56f0519f8ecc8d9096930d92e1a5351ba2
TVP <tomas.victor.pettersson@outlook.com>
2024-09-02 20:31:54 +0200

    Improved comment clarity in 'IValidation.cs' for 'IsInputValid' method

--- Validation/Interfaces/IValidation.cs
+++ Validation/Interfaces/IValidation.cs
@@ -11,7 +11,7 @@ namespace Validation.Interfaces
 		// Checks if the provided username meets the required criteria.
 		bool IsValidUserName(string userName);
 
-		// Validates if the input is a 4-digit number with unique digits.
+		// Validates if the input is a 4-digit number.
 		bool IsInputValid(GameTypes gameType, string input);
 
 		// Checks if the provided guess matches the secret number.
